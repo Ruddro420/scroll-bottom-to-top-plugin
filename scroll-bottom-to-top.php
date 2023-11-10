5f35a4af4edf8269ba7532b6dc29b932438cc740
ruddro42001~ <aliruddro@gmail.com>
2023-11-11 02:00:58 +0600

    plugin making-1

---
+++ scroll-bottom-to-top.php
@@ -0,0 +1,19 @@
+<?php
+/*
+ * Plugin Name:       Bottom To Top
+ * Plugin URI:        https://wordpress.org/plugins/scroll-bottom-to-top/
+ * Description:       "Scroll Bottom to Top" is a lightweight and user-friendly WordPress plugin designed to enhance website navigation.
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Ali Fiad Ruddro
+ * Author URI:        https://ruddro.info/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Update URI:        https://github.com/Ruddro420/scroll-bottom-to-top-plugin
+ * Text Domain:       sbttp
+ * Domain Path:       /languages
+ */
+
+
+?>
